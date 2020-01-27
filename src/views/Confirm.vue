c564e448a13f1f59df665fc9f641c6b5923b7a87
Jack Ammon <jammon17@georgefox.edu>
2020-01-27 13:49:11 -0800

    init

---
+++ src/views/Confirm.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="confirm">
+
+        <v-container>
+            <v-card>
+                <v-card-title class="display-1">
+                    Thanks for confirming!
+
+
+                </v-card-title>
+            </v-card>
+        </v-container>
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+.v-card {
+    margin: 100px auto;
+    padding: 50px;
+    width: 70%;
+    display: table;
+}
+.v-card__title {
+    text-align: center;
+}
+
+</style>
