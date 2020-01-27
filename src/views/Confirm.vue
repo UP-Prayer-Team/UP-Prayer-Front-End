ae1d159ca8ad61aa0e161592ae77accdb313efa7
Ben Garcia <ben.garcia@meetcircle.com>
2020-01-27 14:36:40 -0800

    Added confirmation functionality.

--- src/views/Confirm.vue
+++ src/views/Confirm.vue
@@ -3,20 +3,43 @@
 
         <v-container>
             <v-card>
-                <v-card-title class="display-1">
-                    Thanks for confirming!
-
-
+                <v-card-title class="display-1 text-center">
+                    {{ message }}
                 </v-card-title>
+                <v-card-text class="text-left">
+                    You have signed up to pray at the following times:
+                    <ul>
+                        <li v-for="(slot, i) in confirmedSlots" v-bind:key="i">
+                            {{ slot.year }}-{{ (slot.monthIndex + 1).toString().padStart(2, '0') }}-{{ (slot.dayIndex + 1).toString().padStart(2, '0') }}
+                        </li>
+                    </ul>
+                </v-card-text>
             </v-card>
         </v-container>
-
     </div>
 </template>
 
 <script>
+import UPClient from '../services/UPClient';
+
 export default {
-    
+    data() {
+        return {
+            message: "Confirming...",
+            confirmedSlots: []
+        };
+    },
+    mounted() {
+        let paramString = window.location.hash.split("?")[1];
+        let params = new URLSearchParams(paramString);
+        let confirmationID = params.get("confirmation");
+        UPClient.confirmReservation(confirmationID, slots => {
+            this.message = "Thank you for praying!"
+            this.confirmedSlots = slots;
+        }, message => {
+            console.log("OH NO! Message: " + message);
+        });
+    }
 }
 </script>
 
