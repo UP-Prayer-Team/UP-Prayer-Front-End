06dc83221e81b43cbbef39bd565620e985151f65
Jack Ammon <jammon17@georgefox.edu>
2020-07-24 13:15:06 -0700

    added animate on scroll

--- src/main.js
+++ src/main.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from '@/plugins/vuetify' // path to vuetify export
+import AOS from 'aos'; // animate on scroll
+import 'aos/dist/aos.css';
 
 Vue.config.productionTip = false
+
+AOS.init();
 
 new Vue({
     router,
