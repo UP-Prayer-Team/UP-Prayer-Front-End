a5eb1b1c5decaa60d3455958a4b58f0f8b361343
Jack Ammon <jammon17@georgefox.edu>
2020-08-07 11:09:56 -0700

    switched to vue implementation of google analytics

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import vuetify from '@/plugins/vuetify' // path to vuetify export
 import AOS from 'aos'; // animate on scroll
 import 'aos/dist/aos.css';
 
+import router from './router';
+import VueAnalytics from 'vue-analytics';
+//and then use it in main.js
+Vue.use(VueAnalytics, {
+    id: 'UA-174928847-1',
+    router
+});
+
 Vue.config.productionTip = false
 
 AOS.init();
@@ -14,3 +22,5 @@ new Vue({
     vuetify,
     render: h => h(App)
 }).$mount('#app')
+
+
